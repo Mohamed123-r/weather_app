9dfc0f1d569eeb3e8516e6227d0a656f54544bce
mohamed <mezoattalla123@gmail.com>
2024-02-05 16:34:32 +0200

    trigger request

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -7,25 +11,31 @@ class SearchView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Search a City'),
+        title: const Text('Search a City'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
           child: TextField(
+            onSubmitted: (value) async {
+              WeatherModel weatherModel = await WeatherService().getWeather(
+                  cityName: value);
+            },
             decoration: InputDecoration(
-              contentPadding: EdgeInsets.symmetric(
+              contentPadding: const EdgeInsets.symmetric(
                 vertical: 32,
                 horizontal: 12,
               ),
-                hintText: 'Enter City Name',
-                label: Text('Search'),
-                suffixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                    borderSide: BorderSide(
-                      color: Colors.green,
-                    ))),
+              hintText: 'Enter City Name',
+              label: const Text('Search'),
+              suffixIcon: const Icon(Icons.search),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+                borderSide: const BorderSide(
+                  color: Colors.green,
+                ),
+              ),
+            ),
           ),
         ),
       ),
